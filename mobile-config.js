64e95b92a518480e42763b76741cccd013f37aa8
Gabriel Almeida <gabriel.almeida.rj@gmail.com>
2014-12-22 11:17:34 -0200

    Fixed splash images location

--- mobile-config.js
+++ mobile-config.js
@@ -32,14 +32,14 @@ App.launchScreens({
   'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
 
   // Android
-  'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
-  'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
-  'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
-  'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
-  'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
-  'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
-  'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
-  'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
+  //'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
+  //'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
+  //'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
+  //'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
+  //'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
+  //'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
+  //'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
+  //'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
 });
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
